c7c0a1a39b28cb5066c89b2366b89b3401e03074
Matthew Seagren <mattseagren@gmail.com>
2022-05-04 18:47:50 -0500

    Fixed resolution resizing issue. Making micro change to css to make the webpage look more like leagues item builder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import logo from '../logo.svg';
+// import bgImage from '../lolBackground.jpg';
 import '../App.css';
 import React from 'react';
-// import FontAwesomeIcon from 'fontawesome';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen, faBars, faTrash } from "@fortawesome/free-solid-svg-icons";
 
@@ -15,101 +15,104 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <div className="App-body">
-          <p>Body</p>
+      <>
+        {/* <img src={bgImage} className="background-image" alt="lolbackground" /> */}
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <div className="App-body">
+            <p>Body</p>
 
-          <div className="section1">
-            <div className="itemsetname">
-              <div className="pencil-button">
-                <button className="btn-style"><FontAwesomeIcon icon={faPen} className="icons" /></button>
+            <div className="section1">
+              <div className="itemsetname">
+                <div className="pencil-button">
+                  <button className="btn-style"><FontAwesomeIcon icon={faPen} className="icons" /></button>
+                </div>
+                <strong><p>Unnamed Item Set</p></strong>
+                <div className="itemsetswitch-button">
+                  <FontAwesomeIcon icon={faBars} className="icons" />
+                </div>
               </div>
-              <p>Unnamed Item Set</p>
-              <div className="itemsetswitch-button">
-                <FontAwesomeIcon icon={faBars} className="icons" />
+              <div className="level">
+                <strong><p>Level: 1</p></strong>
+                <div className="levelswitch-button">
+                  <FontAwesomeIcon icon={faBars} className="icons" />
+                </div>
+              </div>
+              <div className="section1a">
+                <div className="delete-button">
+                  <button className="btn-style"><FontAwesomeIcon icon={faTrash} className="icons" /></button>
+
+                </div>
+                <div className="save-button">
+                  <button className="btn-save"><b>Save</b></button>
+                </div>
               </div>
             </div>
-            <div className="level">
-              <p>Level: 1</p>
-              <div className="levelswitch-button">
-                <FontAwesomeIcon icon={faBars} className="icons" />
+            <div className="section2">
+              <div className="section2a">
+                <div className="filteroptions">
+                  <p>filteroptions</p>
+                  <div className="foption"><p>filter Option 1</p></div>
+                  <div className="foption"><p>filter Option 2</p></div>
+                  <div className="foption"><p>filter Option 3</p></div>
+                  <div className="foption"><p>filter Option 4</p></div>
+                  <div className="foption"><p>filter Option 5</p></div>
+                  <div className="foption"><p>filter Option 6</p></div>
+                </div>
+                <div className="items">
+                  <p>items</p>
+                  <div className="itemsearch">
+                    <p>item search</p>
+                  </div>
+                  <div className="item"><p>example item 1</p></div>
+                  <div className="item"><p>example item 2</p></div>
+                  <div className="item"><p>example item 3</p></div>
+                  <div className="item"><p>example item 4</p></div>
+                  <div className="item"><p>example item 5</p></div>
+                  <div className="item"><p>example item 6</p></div>
+                </div>
+              </div>
+              <div className="section2b">
+                <div className="section2b1">
+                  <div className="championportrait">
+                    <p>champion Portrait</p>
+                  </div>
+                  <div className="championname">
+                    <p>champion name</p>
+                  </div>
+                </div>
+                <div className="section2b2">
+                  <div className="statblock">
+                    <p>statblock</p>
+                  </div>
+                </div>
+                <div className="section2b3">
+                  <p>selected items</p>
+                  <div className="selecteditem"><p>selected item 1</p></div>
+                  <div className="selecteditem"><p>selected item 2</p></div>
+                  <div className="selecteditem"><p>selected item 3</p></div>
+                  <div className="selecteditem"><p>selected item 4</p></div>
+                  <div className="selecteditem"><p>selected item 5</p></div>
+                  <div className="selecteditem"><p>selected item 6</p></div>
+                </div>
               </div>
             </div>
-            <div className="section1a">
-              <div className="delete-button">
-                <button className="btn-style"><FontAwesomeIcon icon={faTrash} className="icons" /></button>
-
-              </div>
-              <div className="save-button">
-                <button className="btn-save">Save</button>
-              </div>
+            <div className="section3">
+              <div className="abilities"><p>passive</p></div>
+              <div className="abilities"><p>Q</p><div className="abilitylevel"><p>Level switch</p></div></div>
+              <div className="abilities"><p>W</p><div className="abilitylevel"><p>Level switch</p></div></div>
+              <div className="abilities"><p>E</p><div className="abilitylevel"><p>Level switch</p></div></div>
+              <div className="abilities"><p>R</p><div className="abilitylevel"><p>Level switch</p></div></div>
             </div>
           </div>
-          <div className="section2">
-            <div className="section2a">
-              <div className="filteroptions">
-                <p>filteroptions</p>
-                <div className="foption"><p>filter Option 1</p></div>
-                <div className="foption"><p>filter Option 2</p></div>
-                <div className="foption"><p>filter Option 3</p></div>
-                <div className="foption"><p>filter Option 4</p></div>
-                <div className="foption"><p>filter Option 5</p></div>
-                <div className="foption"><p>filter Option 6</p></div>
-              </div>
-              <div className="items">
-                <p>items</p>
-                <div className="itemsearch">
-                  <p>item search</p>
-                </div>
-                <div className="item"><p>example item 1</p></div>
-                <div className="item"><p>example item 2</p></div>
-                <div className="item"><p>example item 3</p></div>
-                <div className="item"><p>example item 4</p></div>
-                <div className="item"><p>example item 5</p></div>
-                <div className="item"><p>example item 6</p></div>
-              </div>
-            </div>
-            <div className="section2b">
-              <div className="section2b1">
-                <div className="championportrait">
-                  <p>champion Portrait</p>
-                </div>
-                <div className="championname">
-                  <p>champion name</p>
-                </div>
-              </div>
-              <div className="section2b2">
-                <div className="statblock">
-                  <p>statblock</p>
-                </div>
-              </div>
-              <div className="section2b3">
-                <p>selected items</p>
-                <div className="selecteditem"><p>selected item 1</p></div>
-                <div className="selecteditem"><p>selected item 2</p></div>
-                <div className="selecteditem"><p>selected item 3</p></div>
-                <div className="selecteditem"><p>selected item 4</p></div>
-                <div className="selecteditem"><p>selected item 5</p></div>
-                <div className="selecteditem"><p>selected item 6</p></div>
-              </div>
-            </div>
-          </div>
-          <div className="section3">
-            <div className="abilities"><p>passive</p></div>
-            <div className="abilities"><p>Q</p><div className="abilitylevel"><p>Level switch</p></div></div>
-            <div className="abilities"><p>W</p><div className="abilitylevel"><p>Level switch</p></div></div>
-            <div className="abilities"><p>E</p><div className="abilitylevel"><p>Level switch</p></div></div>
-            <div className="abilities"><p>R</p><div className="abilitylevel"><p>Level switch</p></div></div>
-          </div>
-        </div>
 
-        <footer className="App-footer">
-          <p>Footer</p>
-        </footer>
-      </div >
+          <footer className="App-footer">
+            <p>Footer</p>
+          </footer>
+        </div >
+      </>
     );
   }
 }
